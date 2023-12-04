ed1d0775438e9b4bd514ae7e5852e5116ed78980
xavierloeraflores <x.loera.tech@gmail.com>
2023-12-03 18:56:19 -0800

    FEAT: HashTable class

--- HashTable.py
+++ HashTable.py
@@ -1,3 +1,5 @@
+capacity = 40
+
 class Node:
     def __init__(self, key, value):
         self.key = key
@@ -5,3 +7,12 @@ class Node:
         self.next = None
 
 
+
+class HashTable:
+    # Constructor w/ initial capacity of 40 packages
+    def __init__(self, cap=capacity):
+        self.cap = cap
+        self.size = 0
+        self.table = []
+        for i in range(cap):
+            self.table.append(None)
