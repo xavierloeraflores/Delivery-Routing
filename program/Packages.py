2d46fc06129dc06a0bc6583ee7059582a22a3942
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-05 07:37:56 -0800

    Packages special cases

--- program/Packages.py
+++ program/Packages.py
@@ -25,5 +25,23 @@ class Package:
     def __str__(self):
         return f"{self.id}, {self.address}, {self.city}, {self.state}, {self.zip}, {self.deadline}, {self.weight}, {self.notes}"
 
-
+    def has_deadline(self):
+        return self.deadline != "EOD"
     
+    def has_notes(self):
+        return self.notes != ""
+    
+    def is_delayed(self):
+        return self.notes == "Delayed on flight---will not arrive to depot until 9:05 am"
+    
+    def is_wrong_address(self):
+        return self.notes == "Wrong address listed"
+
+    def is_truck_2(self):
+        return self.notes == "Can only be on truck 2"
+    
+    def has_wrong_address(self):
+        return self.notes == "Wrong address listed"
+    
+    def is_bundled(self):
+        return "Must be delivered with" in self.notes
