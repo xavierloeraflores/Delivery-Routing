cd395e6b6d43d09dc085381915373d8599142be4
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-05 23:32:51 -0800

    get package status at time funcs

--- program/main.py
+++ program/main.py
@@ -2,5 +2,12 @@
 # Name:         Xavier Alejandro Loera Flores
 # Date:         12/08/2023
 
+def get_all_packages_status_at_time(time):
+    print("This function will print the status of all packages at a given time")
+
+def get_package_status_at_time(package_id, time):
+    print("This function will print the status of a given package at a given time")
+
+
 class Main:
     print("Hello World")
