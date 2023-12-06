958a664ae3fc2351d3d5b10ca9dc80f9ffb1819c
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-05 23:59:28 -0800

    Added exception to interface loop

--- program/main.py
+++ program/main.py
@@ -9,14 +9,25 @@ def get_package_status_at_time(package_id, time):
     print("This function will print the status of a given package at a given time")
 
 def interface_loop():
-
+    try:
+        choice = int(input("Please choose an option:"))
+        if choice not in range(0, 4) or choice == 0:
+            return True
+        elif choice == 1:
+            print("Option 1")
+        elif choice == 2:
+            print("Option 2")
+        elif choice == 3:
+            print("Option 3")
+    except ValueError:
+        print("Invalid input")
     return False
 
 class Main:
-    print("Hello World")
+    print("Package Delivery Routing Program")
     
     finish_program = False
     while not finish_program:
         finish_program = interface_loop()
     
-    print("Goodbye World")
+    print("Closing Program")
