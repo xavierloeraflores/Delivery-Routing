d215a4f300055570490e11811bce9e42cc0918ab
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-07 13:01:04 -0800

    Added setter and getter functions to package status

--- program/DeliveryPackage.py
+++ program/DeliveryPackage.py
@@ -12,3 +12,11 @@ class DeliveryPackage:
     def __init__(self,  package ):
         self.package = package
         self.status = Status.hub
+    def set_status_to_delivered(self):
+        self.status = Status.delivered
+
+    def set_status_to_enroute(self):
+        self.status = Status.enroute
+        
+    def get_status(self):
+        return self.status
