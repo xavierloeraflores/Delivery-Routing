578d65bfe3d26c5debc58fcab449aa5625f66213
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-07 12:49:53 -0800

    FEAT: Status Enum

--- program/DeliveryPackage.py
+++ program/DeliveryPackage.py
@@ -1,6 +1,10 @@
 from Packages import Package
+from enum import Enum
 
-
+class Status(Enum):
+    hub = "At the Hub"
+    enroute = "Enroute"
+    delivered = "Delivered"
 
 class DeliveryPackage:
     def __init__(self,  package ):
