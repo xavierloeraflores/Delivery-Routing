8aee745e93ce68a9b43c5b0739d2dfaafdab41fd
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-07 13:09:21 -0800

    package delivery time

--- program/DeliveryPackage.py
+++ program/DeliveryPackage.py
@@ -1,5 +1,6 @@
 from Packages import Package
 from enum import Enum
+from datetime import time
 
 class Status(Enum):
     hub = "At the Hub"
@@ -9,14 +10,16 @@ class Status(Enum):
 class DeliveryPackage:
     status = Status
     package = Package
+    delivery_time = time
     def __init__(self,  package ):
         self.package = package
         self.status = Status.hub
+        self.delivery_time = None
     def set_status_to_delivered(self):
         self.status = Status.delivered
 
     def set_status_to_enroute(self):
         self.status = Status.enroute
-        
+
     def get_status(self):
         return self.status
