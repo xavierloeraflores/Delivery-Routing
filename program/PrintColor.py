c3a5521737d43ad557185e7db6700392be48a30d
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-08 10:03:55 -0800

    Added colorful terminal print functions

---
+++ program/PrintColor.py
@@ -0,0 +1,17 @@
+# Prints to the terminal in red
+def print_red(text):
+    print("\033[91m {}\033[00m" .format(text))
+
+# Prints to the terminal in green
+def print_green(text):
+    print("\033[92m {}\033[00m" .format(text))
+
+
+# Prints to the terminal in blue
+def print_blue(text):
+    print("\033[96m {}\033[00m" .format(text))
+
+
+# Prints to the terminal in yellow
+def print_yellow(text):
+    print("\033[93m {}\033[00m" .format(text))
