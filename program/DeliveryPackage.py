5797ee22a219a8aa78244756936aa2cc94dad085
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-08 09:25:41 -0800

    Added setter functions

--- program/DeliveryPackage.py
+++ program/DeliveryPackage.py
@@ -15,11 +15,26 @@ class DeliveryPackage:
         self.package = package
         self.status = Status.hub
         self.delivery_time = None
+    
+    def get_status(self):
+        return self.status
+    
+    def get_package(self):
+        return self.package 
+    
+    def get_delivery_time(self):
+        return self.delivery_time
+    
+    # Setters
+
+    # Sets the status of the package to delivered
     def set_status_to_delivered(self):
         self.status = Status.delivered
 
+    # Sets the status of the package to enroute
     def set_status_to_enroute(self):
         self.status = Status.enroute
 
-    def get_status(self):
-        return self.status
+    # Sets the delivery time of the package
+    def set_delivery_time(self, time):
+        self.delivery_time = time
