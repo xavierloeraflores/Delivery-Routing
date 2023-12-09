9e9e5fc099b734d83ef07b7bf982030f665d0d72
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-09 09:45:05 -0800

    CSVReader.py comments

--- program/CSVReader.py
+++ program/CSVReader.py
@@ -1,9 +1,12 @@
 import csv
 
+# CSVReader class to read CSV files
 class CSVReader:
+    # initialize the CSVReader object with a filename
     def __init__(self, filename):
         self.filename = filename
 
+    # read the CSV file
     def read(self):
         with open(self.filename) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
