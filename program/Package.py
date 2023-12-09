ada1a3dfc340d00e7c9506aa8a9dd210de9e79a0
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-09 09:43:17 -0800

    Package.py comments

--- program/Package.py
+++ program/Package.py
@@ -1,6 +1,6 @@
 from CSVReader import CSVReader
 
-
+# Read the package list from the CSV file
 def read_packages():
     reader = CSVReader("PackageFile.csv")
     rows = reader.read()
@@ -10,7 +10,7 @@ def read_packages():
             packages.append(row)
     return packages
 
-
+# Package class for the package objects raw data
 class Package:
     def __init__(self, row):
         self.id = row[0]
@@ -22,47 +22,64 @@ class Package:
         self.weight = row[6]
         self.notes = row[7]
     
+    # string representation of the package
     def __str__(self):
         return f"{self.id}, {self.address}, {self.city}, {self.state}, {self.zip}, {self.deadline}, {self.weight}, {self.notes}"
 
+    # Getters for the package attributes
+
+    # Get the package id
     def get_id(self):
         return self.id
     
+    # Get the package address
     def get_address(self):
         return self.address
     
+    # Get the package city
     def get_city(self):
         return self.city
     
+    # Get the package state
     def get_state(self):
         return self.state
     
+    # Get the package zip
     def get_zip(self):
         return self.zip
     
+    # Get the package deadline
     def get_deadline(self):
         return self.deadline
     
+    # Get the package weight
     def get_weight(self):
         return self.weight
     
+    # Get the package notes
     def get_notes(self):
         return self.notes
 
+    # Boolean check for deadline
     def has_deadline(self):
         return self.deadline != "EOD"
     
+    # Boolean check for notes
     def has_notes(self):
         return self.notes != ""
     
+    # Boolean check for delayed package
     def is_delayed(self):
         return self.notes == "Delayed on flight---will not arrive to depot until 9:05 am"
 
+    # Boolean check for truck 2
     def is_truck_2(self):
         return self.notes == "Can only be on truck 2"
     
+    # Boolean check for wrong address
     def has_wrong_address(self):
         return self.notes == "Wrong address listed"
     
+    # Boolean check for must be delivered with
     def is_bundled(self):
         return "Must be delivered with" in self.notes
