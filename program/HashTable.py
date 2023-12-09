0ee84c2985e532877b6be41ebbaa022ba4489fa7
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-09 09:53:38 -0800

    Renamed variables

--- program/HashTable.py
+++ program/HashTable.py
@@ -14,9 +14,9 @@ class HashTable:
         self.cap = cap
         self.size = 0
         self.table = []
-        for i in range(cap):
+        for _ in range(cap):   
             self.table.append(None)
-    
+        print()
     # length of the table
     def __len__(self):
         return self.size
@@ -32,13 +32,13 @@ class HashTable:
 
     # insert() inserts a new node into the table
     def insert(self, key, value):
-        hashedIndex = self.hash_key(key)
+        hashed_index = self.hash_key(key)
         key_value_node = Node(key, value)
 
-        if self.table[hashedIndex] == None:
-            self.table[hashedIndex] = key_value_node
+        if self.table[hashed_index] == None:
+            self.table[hashed_index] = key_value_node
         else:
-            cur = self.table[hashedIndex]
+            cur = self.table[hashed_index]
             while cur.next != None:
                 cur = cur.next
             cur.next = key_value_node
@@ -46,10 +46,10 @@ class HashTable:
 
     # update() updates the value of the key
     def update(self, key, value):
-        hashedIndex = self.hash_key(key)
+        hashed_index = self.hash_key(key)
         key_value_node = Node(key, value)
 
-        cur = self.table[hashedIndex]
+        cur = self.table[hashed_index]
         while cur != None:
             if cur.key == key:
                 cur.value = key_value_node.value
@@ -58,8 +58,8 @@ class HashTable:
 
     # get() returns the value of the key
     def get(self, key):
-        hashedIndex = self.hash_key(key)
-        cur = self.table[hashedIndex]
+        hashed_index = self.hash_key(key)
+        cur = self.table[hashed_index]
 
         while cur != None:
             if cur.key == key:
@@ -69,14 +69,14 @@ class HashTable:
 
     # remove() removes the node with the key
     def remove(self, key):
-        hashedIndex = self.hash_key(key)
+        hashed_index = self.hash_key(key)
         prev = None
-        cur = self.table[hashedIndex]
+        cur = self.table[hashed_index]
         
         while cur != None:
             if cur.key == key:
                 if prev == None:
-                    self.table[hashedIndex] = cur.next
+                    self.table[hashed_index] = cur.next
                 else:
                     prev.next = cur.next
                 self.size -= 1
