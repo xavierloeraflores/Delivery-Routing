c4e4633cba83970815a3d077cf904860c2bad1b1
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-09 08:47:47 -0800

    FIX: Added float support for the distances matrix

--- program/DistanceMatrix.py
+++ program/DistanceMatrix.py
@@ -5,7 +5,14 @@ def read_distances():
     rows = reader.read()
     distances = []
     for row in rows:
-        distances.append(row)
+        new_row = []
+        for cell in row:
+            try:
+                cell = float(cell)
+            except ValueError:
+                cell = -1.0
+            new_row.append(cell)
+        distances.append(new_row)
     return distances
 
 def get_distance_between_addresses(address1_id, address2_id, distances):
