7d20db2cecb05a8d81bffe2c9cea2f7022aa9060
xavierloeraflores <x.loera.tech@gmail.com>
2023-12-09 14:58:21 -0800

    Truck travel funcs

--- program/Truck.py
+++ program/Truck.py
@@ -1,4 +1,7 @@
-from datetime import time
+from datetime import time, timedelta
+from DeliveryPackage import DeliveryPackage, Status
+from Utils import get_distance_traveled, get_time_traveled
+from DistanceMatrix import get_distance_between_addresses, get_closest_address, get_address_by_street
 
 #Truck class
 class Truck:
@@ -26,6 +29,7 @@ class Truck:
         self.address = address_id
         self.depart_time = depart_time
         self.packages = packages
+        self.time = depart_time
 
     # string representation of the truck
     def __str__(self): 
@@ -34,8 +38,42 @@ class Truck:
     # Getters for the truck attributes
     def get_depart_time(self):
         return self.depart_time
+    
+    # depart the truck and set the status of the packages to enroute
+    def depart(self, hash_table, time):
+        self.depart_time = time
+        self.load = len(self.packages)
+        for package_id in self.packages:
+            package = hash_table.get(package_id)
+            package.status = Status.enroute
+
+    # get the list of undelivered packages Ids
+    def get_undelivered_packages(self, hash_table):
+        undelivered_packages = []
+        for package_id in self.packages:
+            if hash_table.get(package_id).get_status() == Status.enroute:
+                undelivered_packages.append(package_id)
+        return undelivered_packages
+    
+    # deliver a single package
+    def deliver_package(self, package_id, distances, hash_table):
+        package = hash_table.get(package_id)
+        distance = get_distance_between_addresses(self.address_id, package.address_id, distances) 
+        package.set_status_to_delivered()
+        self.address_id = package.address_id
+        hash_table.update(package_id, package)
+        self.load -= 1
+        self.mileage += distance
+        minutes_traveled = get_time_traveled(distance, self.speed)
+        self.time = self.time+timedelta(minutes=minutes_traveled)
 
 
+    # travel for a given time
+    def travel_for_time_in_min(self, min):
+        self.time = self.time+timedelta(minutes=min)
+        distance_traveled = get_distance_traveled(self.speed, min)
+        self.mileage += distance_traveled
+    
     # set the current address of the truck
     def set_address_id(self, address_id):
         self.address_id = address_id
