f2b4d03da4e8ed5e1675e4258ef20f8a5b0a685c
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-12-09 09:39:43 -0800

    Added comments to Delivery Package

--- program/DeliveryPackage.py
+++ program/DeliveryPackage.py
@@ -2,26 +2,33 @@ from Package import Package
 from enum import Enum
 from datetime import time
 
+# Status Enum that represents the status of the package
 class Status(Enum):
     hub = "At the Hub"
     enroute = "Enroute"
     delivered = "Delivered"
 
+# DeliveryPackage class
 class DeliveryPackage:
     status = Status
     package = Package
     delivery_time = time
+
+    # __init__ Constructor
     def __init__(self,  package ):
         self.package = package
         self.status = Status.hub
         self.delivery_time = None
     
+    # get the status of the package
     def get_status(self):
         return self.status
     
+    # get the package object
     def get_package(self):
         return self.package 
     
+    # get the delivery time of the package
     def get_delivery_time(self):
         return self.delivery_time
     
